82d22429f3d599270c9681c8876072247c4a54f3
Stephany_moreno <stephanymoreno1826@gmail.com>
2025-10-06 18:04:38 -0500

    se trabajo el tercer ejercicio

---
+++ operador3.js
@@ -0,0 +1,19 @@
+//declaracion de variables 
+
+let contador, visitasadicionales, visitasperdidad, contadorfinal;
+
+//datos de entrada
+contador = 100;
+visitasadicionales = 25;
+visitasperdidad = 10;
+
+//parseo de datos
+//no es necesario
+
+//proceso matematico
+
+contador = contador + visitasadicionales;
+contador = contador -visitasperdidad;
+contadorfinal = contador * 2;
+
+alert("el valro final del contader de visitas es: "+ contadorfinal)
